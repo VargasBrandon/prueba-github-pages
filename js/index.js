a68790a48b2b12f192d9f5d534bd2a82b23931cc
VargasBrandon <56104870+VargasBrandon@users.noreply.github.com>
2021-06-01 18:17:41 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,42 @@
+function main(){
+
+    var btnAbrirr = document.getElementById("btnAbrir");
+    var btnAnalizarr = document.getElementById("btnAnalizar");
+    var btnReporteErrorr = document.getElementById("btnReporteError");
+    var btnCopiasAnalizarr = document.getElementById("btnCopiasAnalizar");
+    var btnAbrirr2 = document.getElementById("btnAbrir2");
+
+
+    btnAnalizarr.onclick = function(e){
+        // textoArchivo = "";
+        // textoArchivo = document.getElementById("txtCampo").value;
+        // document.getElementById('ulAgregar').innerHTML = '';
+        // AnalizarTexto(textoArchivo);
+    }
+    
+    btnAbrirr.onclick = function(e){     
+        // LimpiarTodo();
+        // AbrirArchivo();
+    }
+    btnReporteErrorr.onclick = function(e){  
+        // textoErrores = "";
+        // LlamarError("s");
+        // alert("!Reporte de Error Generado!");
+    }
+
+    btnCopiasAnalizarr.onclick = function(e){  
+        // LimpiarTabla1();
+        // LimpiarTabla2();
+        // LimpiarTabla3();
+        // textoArchivoCopia = "";
+        // textoArchivoCopia = document.getElementById("txtTraduccion").value;
+        // //AgregarClases("s");
+        // AnalizarTextoCopia(textoArchivoCopia);
+    }
+
+    btnAbrirr2.onclick = function(e){  
+        //AbrirArchivo2();
+    }
+
+
+}
